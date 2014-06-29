0ea5e8d2e870b273eb19111f4fe925dac55c7c74
Pieter Provoost <pieterprovoost@gmail.com>
2014-06-29 14:10:44 +0200

    refactoring

--- test/t.js
+++ test/t.js
@@ -1,7 +1,7 @@
 describe('jerzy.StudentT', function() {
 	var first = new jerzy.Vector([26, 21, 22, 26, 19, 22, 26, 25, 24, 21, 23, 23, 18, 29, 22]);
 	var second = new jerzy.Vector([18, 23, 21, 20, 20, 29, 20, 16, 20, 26, 21, 25, 17, 18, 19]);
-	var st = new jerzy.StudentT(first, second);
+	var st = jerzy.StudentT.test(first, second);
 	describe("#df", function() {
 		it("should return the correct degrees of freedom", function() {
 			assert.equal(st.df, 28);
