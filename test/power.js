e5f6b37d20b6650bfdc7b9df56ea6df97fab85f2
Pieter Provoost <pieterprovoost@gmail.com>
2014-10-21 23:46:25 +0200

    minor fixes

--- test/power.js
+++ test/power.js
@@ -1,5 +1,4 @@
 describe('jerzy.Power', function() {
-
 	describe("#sampleSize", function() {
 		it("should return the correct sample size", function() {
 			assert.closeTo(jerzy.Power.sampleSize(0.05, 0.8, 0.72, 0.15), 362, 1);
